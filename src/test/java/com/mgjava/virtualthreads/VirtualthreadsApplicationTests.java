1efe35f101fa795aa33e0c305644994d23d3a190
Murugesh Govindhappa <murugesh2311@gmail.com>
2025-05-20 18:59:09 +0530

    Initial commit

---
+++ src/test/java/com/mgjava/virtualthreads/VirtualthreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgjava.virtualthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
